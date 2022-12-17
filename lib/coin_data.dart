e2dd91dec07d2d6e24ef11dfd2310435741af8a9
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-12-17 08:49:01 +0200

    2. Create getCoinData() method that returns the price data

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,24 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinApiKey = 'D4265300-3534-4CCF-AFED-B89FF204C2AD';
+const coinApiHost = 'rest.coinapi.io';
+const coinApiPath = '/v1/exchangerate/BTC/';
+
+class CoinData {
+  Future<double> getCoinData({required String currency}) async {
+    final response = await get(
+      Uri.https(coinApiHost, '$coinApiPath' '$currency'),
+      headers: {'X-CoinAPI-Key': coinApiKey},
+    );
+
+    if (response.statusCode == 200) {
+      final decodedData = json.decode(response.body);
+      return decodedData['rate'] as double;
+    } else {
+      print(response.statusCode);
+      //Optional: throw an error if our request fails.
+      throw 'Problem with the get request';
+    }
+  }
+}
